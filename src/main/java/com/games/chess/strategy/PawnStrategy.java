b69c7370624742313785f2a47cf9b735a5767823
airetOK <sergey.bezrukavyy@gmail.com>
2022-08-20 15:28:43 +0300

    Add validation method to strategies

--- src/main/java/com/games/chess/strategy/PawnStrategy.java
+++ src/main/java/com/games/chess/strategy/PawnStrategy.java
@@ -7,10 +7,20 @@ public class PawnStrategy extends GameStrategy {
 
     @Override
     public void execute(Action action, Position position) {
+        checkActionValidity(action);
         if (action.equals(Action.MOVE)) {
             this.getChessman().move(position);
         } else {
             this.getChessman().attack(position);
+        }
+    }
+
+    @Override
+    public void checkActionValidity(Action action) {
+        if (action.equals(Action.MOVE)) {
+            //validation logic here
+        } else {
+            //validation logic here
         }
     }
 }
