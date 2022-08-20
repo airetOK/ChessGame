352e61d7bafb370939200e6c1344bfd2ac39c1d2
airetOK <sergey.bezrukavyy@gmail.com>
2022-08-20 13:45:41 +0300

    Add strategies and entities Player and Action

---
+++ src/main/java/com/games/chess/strategy/KingStrategy.java
@@ -0,0 +1,16 @@
+package com.games.chess.strategy;
+
+import com.games.chess.entity.Action;
+import com.games.chess.entity.Position;
+
+public class KingStrategy extends GameStrategy {
+
+    @Override
+    public void execute(Action action, Position position) {
+        if (action.equals(Action.MOVE)) {
+            this.getChessman().move(position);
+        } else {
+            this.getChessman().attack(position);
+        }
+    }
+}
